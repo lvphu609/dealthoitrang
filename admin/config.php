e1257a97a9fae69b1bf5a8aba9398b3ce14a36aa
Le Vinh Phu <lvphu609@gmail.com>
2015-01-12 23:45:14 +0700

    dealthoitrang first source

---
+++ admin/config.php
@@ -0,0 +1,30 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/dealthoitrang/admin/');
+define('HTTP_CATALOG', 'http://localhost/dealthoitrang/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/dealthoitrang/admin/');
+define('HTTPS_CATALOG', 'http://localhost/dealthoitrang/');
+
+// DIR
+define('DIR_APPLICATION', 'C:\wamp\www\dealthoitrang/admin/');
+define('DIR_SYSTEM', 'C:\wamp\www\dealthoitrang/system/');
+define('DIR_LANGUAGE', 'C:\wamp\www\dealthoitrang/admin/language/');
+define('DIR_TEMPLATE', 'C:\wamp\www\dealthoitrang/admin/view/template/');
+define('DIR_CONFIG', 'C:\wamp\www\dealthoitrang/system/config/');
+define('DIR_IMAGE', 'C:\wamp\www\dealthoitrang/image/');
+define('DIR_CACHE', 'C:\wamp\www\dealthoitrang/system/cache/');
+define('DIR_DOWNLOAD', 'C:\wamp\www\dealthoitrang/system/download/');
+define('DIR_UPLOAD', 'C:\wamp\www\dealthoitrang/system/upload/');
+define('DIR_LOGS', 'C:\wamp\www\dealthoitrang/system/logs/');
+define('DIR_MODIFICATION', 'C:\wamp\www\dealthoitrang/system/modification/');
+define('DIR_CATALOG', 'C:\wamp\www\dealthoitrang/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'dealthoitrang');
+define('DB_PREFIX', 'deal_');
---
+++ config.php
@@ -0,0 +1,27 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/dealthoitrang/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/dealthoitrang/');
+
+// DIR
+define('DIR_APPLICATION', 'C:\wamp\www\dealthoitrang/catalog/');
+define('DIR_SYSTEM', 'C:\wamp\www\dealthoitrang/system/');
+define('DIR_LANGUAGE', 'C:\wamp\www\dealthoitrang/catalog/language/');
+define('DIR_TEMPLATE', 'C:\wamp\www\dealthoitrang/catalog/view/theme/');
+define('DIR_CONFIG', 'C:\wamp\www\dealthoitrang/system/config/');
+define('DIR_IMAGE', 'C:\wamp\www\dealthoitrang/image/');
+define('DIR_CACHE', 'C:\wamp\www\dealthoitrang/system/cache/');
+define('DIR_DOWNLOAD', 'C:\wamp\www\dealthoitrang/system/download/');
+define('DIR_UPLOAD', 'C:\wamp\www\dealthoitrang/system/upload/');
+define('DIR_MODIFICATION', 'C:\wamp\www\dealthoitrang/system/modification/');
+define('DIR_LOGS', 'C:\wamp\www\dealthoitrang/system/logs/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'dealthoitrang');
+define('DB_PREFIX', 'deal_');
